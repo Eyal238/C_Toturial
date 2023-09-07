4342c2afde755401ffb3ab981f2ac7aab930acf4
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-09-07 10:14:11 +0300

    Update shiur1.cpp

--- shiur1.cpp
+++ shiur1.cpp
@@ -1,1 +1,14 @@
-
+// 1.
+#define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
+int main()
+{
+	char let1,let2,let3;
+		
+	printf("Enter 3 letters:\n");
+	scanf("%s",&let1);
+	scanf("%s",&let2);
+	scanf("%s",&let3);
+	
+	printf("%c, %c, %c,",let3,let2,let1);
+}
