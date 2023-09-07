be7b4c6372d87a8f99eea43304503cf7e8badc9e
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-09-07 11:44:27 +0300

    Update shiur1.cpp

--- shiur1.cpp
+++ shiur1.cpp
@@ -12,3 +12,20 @@ int main()
 	
 	printf("%c, %c, %c,",let3,let2,let1);
 }
+//2.
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int x, y;
+	int p, s;
+	printf("Enter the length of the rectangle:");
+	scanf("%d", &x);
+	printf("Enter the width of the rectangle:");
+	scanf("%d", &y);
+	p = 2 * (x + y);
+	s = x * y;
+	printf("The perimeter of the rectangle is %d:\n",p);
+	printf("The area of the rectangle is %d:",s); 
+}
