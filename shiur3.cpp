566a65f9ad1a1ce9bf5f21263a5fbb4908f4c361
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-09-15 12:14:32 +0300

    Update shiur3.cpp

--- shiur3.cpp
+++ shiur3.cpp
@@ -31,3 +31,32 @@ int main()
     average = sum / 8;
     printf("The  average is %f.",average);
 }
+//3.
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+   int i=0, number;
+   int count=10000;
+   int ans=0;
+   printf("Enter your number:");
+   scanf("%d",&number);
+   printf("your number is %d\n",number);
+   while(number>10)
+   {
+       if((number/count)==(number%10))
+       {
+           ans=1;
+       }
+       else
+       {
+           ans=0;
+           break;
+       }
+       number=number%count;
+       number=number%10;
+       count=count/100;
+  }
+   printf( "The value is %d palindrom",ans);
+}
+
