fb9f8f080502701c3b85fe37978131bfe0e75054
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-09-26 07:57:48 +0300

    Update shiur4.cpp

--- shiur4.cpp
+++ shiur4.cpp
@@ -14,3 +14,30 @@ int main()
 {
     f1(5,2);
 }
+//2.
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void f2(int x)
+{
+    int i;
+    int a = 1, b = 1;
+    int res = 0;
+    for (i = 0; i <= x; i++)
+    {
+        if((x==a)||(x==b))
+        {
+            res = 1;
+            break;
+        }
+        a = a + b;
+        b = a + b;
+    }
+    if(res==1)
+        printf("true");
+    else
+        printf("false");
+}
+int main()
+{
+    f2(12);
+}
