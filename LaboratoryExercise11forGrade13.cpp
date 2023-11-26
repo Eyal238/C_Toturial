69495e4bc19e92f7f054899c0378e63ec2f40ae5
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-11-26 13:15:10 +0200

    Update LaboratoryExercise.cpp

--- LaboratoryExercise11forGrade13.cpp
+++ LaboratoryExercise11forGrade13.cpp
@@ -1,3 +1,28 @@
+//תרגיל 3
+//    כתוב תוכנית הקולטת מהמשתמש מספרים שלמים חיוביים, מספר הקלטים לא ידוע. פעולת הקלט תיפסק כאשר מספר שלילי כלשהו
+//    התוכנית תציג כפלט על המסך כמה מהמספרים שנקלטו גדולים מ-50 וכמה מהמספרים קטנים מ-50.
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<math.h>
+void howMany50a()
+{
+	int num;
+	int countBig = 0, countSmall = 0;
+	printf("Enter your number:");
+	scanf("%d", &num);
+	while (num >= 0)
+	{
+		if (num >= 50)
+			countBig++;
+		else
+			countSmall++;
+		printf("Enter your number:");
+		scanf("%d", &num);
+	}
+	printf("%d numbers less than fifty\n", countSmall);
+	printf("%d numbers greater than fifty", countBig);
+}
+//תרגיל 11
 // כתבו תוכנית המגדירה מערך דו מימדי בגודל 10 על 10 מסוג מספרים שלמים, על התוכנית לבדוק האם האלכסון 
 // הראשי שווה לאלכסון המשני, על התוכנית להדפיס הודעה מתאימה.
 
